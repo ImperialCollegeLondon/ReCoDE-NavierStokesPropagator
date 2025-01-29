f19ee555999001ca196188f01afbb7859b56ac51
Zhao <weezhao@live.com>
2025-01-29 18:02:00 +0000

    chore : rename package

--- src/NavierStokesPropagators.jl
+++ src/NavierStokesPropagators.jl
@@ -1,4 +1,4 @@
-module NavierStokesPropagator
+module NavierStokesPropagators
 
 #=
 Package
@@ -27,7 +27,8 @@ Public API
 =#
 
 export BoundTuple
-export SimulationCondition, planeCouetteFlow, planePoiseuilleFlow
+export SimulationCondition
+export planeCouetteFlow, planePoiseuilleFlow
 
 export Transformer
 export y2y_F!, y_F2y!
@@ -370,22 +371,22 @@ function init_flowfield!(
 
     # Enforce Boundary Condition
     state.u[1, :, :] .= 0.0
-    state.u[1, 1, 1] = sim_cond.bound_cond[(vel = 'u', wall = 'l')]
+    state.u[1, 1, 1] = sim_cond.bound_cond[(vel='u', wall='l')]
 
     state.u[end, :, :] .= 0.0
-    state.u[end, 1, 1] = sim_cond.bound_cond[(vel = 'u', wall = 'u')]
+    state.u[end, 1, 1] = sim_cond.bound_cond[(vel='u', wall='u')]
 
     state.v[1, :, :] .= 0.0
-    state.v[1, 1, 1] = sim_cond.bound_cond[(vel = 'v', wall = 'l')]
+    state.v[1, 1, 1] = sim_cond.bound_cond[(vel='v', wall='l')]
 
     state.v[end, :, :] .= 0.0
-    state.v[end, 1, 1] = sim_cond.bound_cond[(vel = 'v', wall = 'u')]
+    state.v[end, 1, 1] = sim_cond.bound_cond[(vel='v', wall='u')]
 
     state.w[1, :, :] .= 0.0
-    state.w[1, 1, 1] = sim_cond.bound_cond[(vel = 'w', wall = 'l')]
+    state.w[1, 1, 1] = sim_cond.bound_cond[(vel='w', wall='l')]
 
     state.w[end, :, :] .= 0.0
-    state.w[end, 1, 1] = sim_cond.bound_cond[(vel = 'w', wall = 'u')]
+    state.w[end, 1, 1] = sim_cond.bound_cond[(vel='w', wall='u')]
 
     return nothing
 
