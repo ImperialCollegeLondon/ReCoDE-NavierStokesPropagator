b553107d421378544c70e2d84f9b64c57bf0f8a6
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2025-01-29 14:25:20 +0000

    feat: add documenter make

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+# Documenter make.jl
+
+#=
+Package
+=#
+
+include("../src/NavierStokesPropagator.jl")
+using .NavierStokesPropagator
+
+using Documenter
+
+#=
+Make Documenter
+=#
+
+makedocs(
+    sitename = "Navier-Stokes Propagator",
+    pages = [
+        "Home" => "index.md",
+        #"Simulation Constructs" => Any["sim/States.md",],
+        #"Simulation Utilities" => Any["util/LinearSolvers.md", "util/Transformations.md"],
+    ],
+)
---
+++ docs/src/index.md
@@ -0,0 +1,3 @@
+# Navier-Stokes Propagator
+
+Figure out how to include README.md
